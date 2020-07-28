6571ceae9ab0e422b09bbd7b182cfc801be7f637
Allan Martins <allanmartins@id.uff.br>
2020-07-28 17:43:16 -0300

    btn for 'cadastro' and new carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
-// import ButtonLink from './components/ButtonLink';
 import Button from "../Button/index";
 
 function Menu(){
     return(
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="AluraFlix logo"></img>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">Novo vídeo</Button>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">Novo vídeo</Button>
         </nav>
     )
 }
